48ef8635e12ffe31bf34c6c280a93b6fdd9bde02
Timothy Goon <t.kgoonx@gmail.com>
2020-02-16 20:35:04 -0500

    Added createRoomsList function.

--- hw4/src/Production.cpp
+++ hw4/src/Production.cpp
@@ -6,20 +6,74 @@
  */
 
 #include "Production.h"
-#include <stdbool.h>
 
-Production::Production() {
-	// TODO Auto-generated constructor stub
+// Creates a linked list of rooms.
+LinkedList* Production::createRoomsList(){
+    // Open file with room data and make new linked list.
+    FILE* file = fopen("MansionRooms.txt", "r");
+    auto list = new LinkedList();
 
-}
+    int numRooms;
+    fscanf(file,"%d", &numRooms);
 
-Production::~Production() {
-	// TODO Auto-generated destructor stub
+    // Add all of the rooms to the list.
+    for(int i=0; i<numRooms; i++){
+        int roomNum = 0;
+        char roomName[FILENAMELENGTHALLOWANCE];
+        int numClues = 0;
+
+        fscanf(file, "%d", &roomNum);
+        fscanf(file, "%s", roomName);
+        fscanf(file, "%d", &numClues);
+
+        std::string name = roomName;
+        list->savePayload(new Room(roomNum, &name, numClues));
+    }
+
+    fclose(file);
+
+    return list;
 }
 
 bool Production::prod(int argc, char* argv[])
 {
 	bool answer = true;
+
+    char breadth[20];
+    int startingRoom = 0;
+    int numRooms = INT_MAX;
+    int clueLimit = INT_MAX;
+    printf("Inspector Gompei:\n\tDo you want to do a thorough examination of the entire mansion, \n"
+           "\ta local search of a set number of rooms, or until you have found \n"
+           "\ta certain number of clues?(thorough/local/clues)\n\nAnswer: ");
+    scanf("%s", breadth);
+    if(strcmp(breadth, "thorough") == 0){
+        printf("%s", breadth);
+    }
+    else if(strcmp(breadth, "local") == 0) {
+        printf("%s", breadth);
+        printf("Inspector Gompei:\n\tHow many rooms do you want to search?(1-12)"
+               "\n\nAnswer: ");
+        scanf("%d", &numRooms);
+
+    }
+    else if(strcmp(breadth, "clues") == 0){
+        printf("%s",breadth);
+        printf("Inspector Gompei:\n\tAfter how many clues would you like to stop your search?"
+               "\n\nAnswer: ");
+        scanf("%d", &clueLimit);
+
+    }
+    else{
+        printf("Invalid Input.");
+        return 0;
+    }
+
+    printf("Inspector Gompei:\n\tWhere do you want to start your search?(RoomNumber 0-11)"
+           "\n\nAnswer: ");
+    scanf("%d", &startingRoom);
+
+
 	return answer;
 
 }
--- hw4/src/Production.h
+++ hw4/src/Production.h
@@ -8,11 +8,17 @@
 #ifndef PRODUCTION_H_
 #define PRODUCTION_H_
 
+#include <cstdio>
+#include <climits>
+#include <cstring>
+#include "LinkedList.h"
+
+#define FILENAMELENGTHALLOWANCE 50
+
 class Production {
 public:
-	Production();
-	virtual ~Production();
-	bool prod(int argc, char* argv[]);
+	static bool prod(int argc, char* argv[]);
+    static LinkedList* createRoomsList();
 };
 
 #endif /* PRODUCTION_H_ */
