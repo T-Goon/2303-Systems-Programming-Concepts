dd233ec019bc610035a3201ac5b4399df2e784b8
Timothy Goon <t.kgoonx@gmail.com>
2020-02-22 21:08:10 -0500

    Initial Commit

---
+++ hw5/src/Student.cpp
@@ -0,0 +1,18 @@
+/*
+ * Student.cpp
+ *
+ *  Created on: Feb 21, 2020
+ *      Author: michaelengling
+ */
+
+#include "Student.h"
+
+Student::Student() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Student::~Student() {
+	// TODO Auto-generated destructor stub
+}
+
